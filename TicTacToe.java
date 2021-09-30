f1bce3ca7de8946b7bcbdcfcdabff80c9cb69168
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-09-30 19:44:59 +0530

    ADD:newScoreBoard

---
+++ TicTacToe.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.app;
+
+import java.util.*;
+
+public class TicTacToe {
+
+	public static void scoreBoard() {
+		char[] Board = new char[10];
+		for (int i = 0; i < 10; i++) {
+			Board[i] = ' ';
+		}
+		
+
+	}
+
+	public static void main(String[] args) {
+
+		TicTacToe.scoreBoard();
+	}
+
+}
